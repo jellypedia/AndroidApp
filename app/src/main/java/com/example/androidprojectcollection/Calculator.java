da12c1dcf4677b10459bca6bf3c768f2b588c26b
LAB11_USER ON WS42 <L11X13W42@citulab.net>
2024-02-28 14:33:02 +0800

    testing

--- app/src/main/java/com/example/androidprojectcollection/Calculator.java
+++ app/src/main/java/com/example/androidprojectcollection/Calculator.java
@@ -3,12 +3,41 @@ package com.example.androidprojectcollection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Calculator extends AppCompatActivity {
+    Button btnOne;
+    Button btnTwo;
+    Button btnThree;
+    Button btnFour;
+    Button btnFive;
+    Button btnSix;
+    Button btnSeven;
+    Button btnEight;
+    Button btnNine;
+    Button btnZero;
+    Button btnAdd, btnMinus, btnMultiply, btnDivide, btnDecimal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        btnOne = findViewById(R.id.btnOne);
+        btnTwo = findViewById(R.id.btnTwo);
+        btnThree = findViewById(R.id.btnThree);
+        btnFour = findViewById(R.id.btnFour);
+        btnFive = findViewById(R.id.btnFive);
+        btnSix = findViewById(R.id.btnSix);
+        btnSeven = findViewById(R.id.btnSeven);
+        btnEight = findViewById(R.id.btnEight);
+        btnNine = findViewById(R.id.btnNine);
+        btnZero = findViewById(R.id.btnZero);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnDecimal = findViewById(R.id.btnDecimal);
+
+
     }
 }
