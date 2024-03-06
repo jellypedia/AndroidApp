b4dfa34df3348954f0be877decb509052a62b16a
angel <angelxo.25@gmail.com>
2024-03-06 14:53:40 +0800

    Calculator updated

--- app/src/main/java/com/example/androidprojectcollection/Calculator.java
+++ app/src/main/java/com/example/androidprojectcollection/Calculator.java
@@ -3,20 +3,20 @@ package com.example.androidprojectcollection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Stack;
 
 public class Calculator extends AppCompatActivity {
-    Button btnOne;
-    Button btnTwo;
-    Button btnThree;
-    Button btnFour;
-    Button btnFive;
-    Button btnSix;
-    Button btnSeven;
-    Button btnEight;
-    Button btnNine;
-    Button btnZero;
-    Button btnAdd, btnMinus, btnMultiply, btnDivide, btnDecimal;
+    Button btnOne, btnTwo, btnThree, btnFour, btnFive;
+    Button btnSix, btnSeven, btnEight, btnNine, btnZero;
+    Button btnAdd, btnMinus, btnMultiply, btnDivide, btnDecimal, btnEquals;
+    TextView txtResult, txtEqua;
+    Stack<Object> calcu = new Stack<>();
+    int num, result;
+    StringBuilder str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +37,75 @@ public class Calculator extends AppCompatActivity {
         btnMultiply = findViewById(R.id.btnMultiply);
         btnDivide = findViewById(R.id.btnDivide);
         btnDecimal = findViewById(R.id.btnDecimal);
+        btnEquals = findViewById(R.id.btnEquals);
 
+        txtResult = findViewById(R.id.txtResult);
+        txtEqua = findViewById(R.id.txtEqua);
+
+
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtResult.append("1");
+            }
+        });
+
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtResult.append("2");
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num = Integer.parseInt(txtResult.getText().toString());
+                str = new StringBuilder();
+                if(str.charAt(str.length()-1) == '*' || str.charAt(str.length()-1) == '-' || str.charAt(str.length()-1) == '/') {
+                    str = str.replace(str.length()-1,str.length(),"+");
+                    txtEqua.setText(str.toString());
+                }
+//11+ = 3
+//                txtEqua.append((num)+"+");
+//                txtResult.setText("");
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num = Integer.parseInt(txtResult.getText().toString());
+//                calculate(num,"-");
+
+                txtEqua.append((num)+"-");
+                txtResult.setText("");
+            }
+        });
+
+        btnEquals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtResult.append(""+result);
+            }
+        });
 
     }
+
+    void calculate(int num, String op) {
+        if(calcu.peek() instanceof String) { //operator sya
+            switch(op) {
+                case "+":
+                    calcu.pop();
+                    result = num+(Integer)calcu.peek();
+                    break;
+                case "-":
+                    break;
+            }
+        }
+
+        calcu.push(num);
+        calcu.push(op);
+    }
+
 }
