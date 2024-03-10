1e4e6ca182e980c786c5be972175d455fa4b4656
Angel <angelxo.25@gmail.com>
2024-03-10 15:28:15 +0800

    fucking sequential grah grah grah

--- app/src/main/java/com/example/androidprojectcollection/Calculator.java
+++ app/src/main/java/com/example/androidprojectcollection/Calculator.java
@@ -2,21 +2,25 @@ package com.example.androidprojectcollection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.List;
 import java.util.Stack;
 
 public class Calculator extends AppCompatActivity {
     Button btnOne, btnTwo, btnThree, btnFour, btnFive;
     Button btnSix, btnSeven, btnEight, btnNine, btnZero;
     Button btnAdd, btnMinus, btnMultiply, btnDivide, btnDecimal, btnEquals;
-    TextView txtResult, txtEqua;
-    Stack<Object> calcu = new Stack<>();
-    int num, result;
-    StringBuilder str;
+    TextView txtResult, txtEqua, txtTest;
+    Stack<String> calcu = new Stack<String>();
+    int equaLen, result; //equaLen len until sa op. 123+ is len: 4
+    StringBuilder strEqua, strResult;
+    boolean isOp = false;
+    String op;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,45 +45,143 @@ public class Calculator extends AppCompatActivity {
 
         txtResult = findViewById(R.id.txtResult);
         txtEqua = findViewById(R.id.txtEqua);
+        txtTest = findViewById(R.id.txtTesting);
 
+        btnDecimal.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                strEqua = new StringBuilder(txtEqua.getText().toString());
+                strResult = new StringBuilder(txtResult.getText().toString());
+
+                if(!txtResult.getText().toString().contains(".")) { //walay decimal, igo ra-add
+                    if(txtResult.getText().toString().isEmpty()) {
+                        txtResult.setText(strResult.toString() + "0.");
+                        txtEqua.setText(strEqua.toString() + "0.");
+                    } else {
+                        txtResult.setText(strResult.toString() + ".");
+                        txtEqua.setText(strEqua.toString() + ".");
+                    }
+                } else if (txtResult.getText().toString().contains(".") && strEqua.charAt(strEqua.length() - 1) == '.') {
+                    strEqua.deleteCharAt(strEqua.length()-1);
+                    strResult.deleteCharAt(strResult.length()-1);
+
+                    txtResult.setText(strResult.toString());
+                    txtEqua.setText(strEqua.toString());
+                }
+            }
+        });
 
         btnOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txtResult.append("1");
+                btnNumAppend(1);
             }
         });
 
         btnTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txtResult.append("2");
+                btnNumAppend(2);
+            }
+        });
+
+        btnThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnNumAppend(3);
+            }
+        });
+
+        btnFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnNumAppend(4);
+            }
+        });
+
+        btnFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnNumAppend(5);
+            }
+        });
+
+        btnSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnNumAppend(6);
+            }
+        });
+
+        btnSeven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnNumAppend(7);
+            }
+        });
+
+        btnEight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnNumAppend(8);
+            }
+        });
+
+        btnNine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnNumAppend(9);
+            }
+        });
+
+        btnZero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnNumAppend(0);
             }
         });
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                num = Integer.parseInt(txtResult.getText().toString());
-                str = new StringBuilder();
-                if(str.charAt(str.length()-1) == '*' || str.charAt(str.length()-1) == '-' || str.charAt(str.length()-1) == '/') {
-                    str = str.replace(str.length()-1,str.length(),"+");
-                    txtEqua.setText(str.toString());
-                }
-//11+ = 3
-//                txtEqua.append((num)+"+");
-//                txtResult.setText("");
+                strEqua = new StringBuilder(txtEqua.getText().toString());
+
+                calcu.push(txtResult.getText().toString());
+
+                operator(strEqua,'+');
+                op = "+";
             }
         });
 
         btnMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                num = Integer.parseInt(txtResult.getText().toString());
-//                calculate(num,"-");
+                strEqua = new StringBuilder(txtEqua.getText().toString());
+                calcu.push(txtResult.getText().toString());
 
-                txtEqua.append((num)+"-");
-                txtResult.setText("");
+                operator(strEqua,'-');
+
+            }
+        });
+
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                strEqua = new StringBuilder(txtEqua.getText().toString());
+                calcu.push(txtResult.getText().toString());
+
+                operator(strEqua,'*');
+            }
+        });
+
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                strEqua = new StringBuilder(txtEqua.getText().toString());
+                calcu.push(txtResult.getText().toString());
+
+                operator(strEqua,'/');
             }
         });
 
@@ -92,20 +194,43 @@ public class Calculator extends AppCompatActivity {
 
     }
 
-    void calculate(int num, String op) {
-        if(calcu.peek() instanceof String) { //operator sya
-            switch(op) {
-                case "+":
-                    calcu.pop();
-                    result = num+(Integer)calcu.peek();
-                    break;
-                case "-":
-                    break;
-            }
+    public void operator(StringBuilder str, char op) {
+        if(!Character.isDigit(str.charAt(str.length()-1)) && str.charAt(str.length()-1) != '.') {
+            str.setCharAt(str.length()-1,op);
+        } else {
+            str.append(op);
         }
-
-        calcu.push(num);
-        calcu.push(op);
+        txtResult.setText("");
+        txtEqua.setText(str.toString());
+        isOp = true;
     }
 
+    public void btnNumAppend(int num) {
+        txtResult.append(""+num);
+        txtEqua.append(""+num);
+
+        if(calcu.size() == 1) {
+            calcuSequential(op);
+        }
+    }
+
+    public void calcuSequential(String op) {
+
+        String num1, num2;
+        float result = 0;
+
+        switch(op) {
+            case "+":
+                num2 = txtResult.getText().toString();
+                num1 = calcu.pop();
+                result = Float.parseFloat(num1) + Float.parseFloat(num2);
+                break;
+        }
+
+        calcu.push(String.valueOf(result));
+        System.out.println(result);
+        txtResult.setText("");
+        txtResult.setText(result+"");
+
+    }
 }
