e7712264768485663c048f63d55985226a8360f2
Angel <angelxo.25@gmail.com>
2024-03-12 23:14:35 +0800

    Layout might be ok

--- app/src/main/java/com/example/androidprojectcollection/Calculator.java
+++ app/src/main/java/com/example/androidprojectcollection/Calculator.java
@@ -9,7 +9,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Stack;
 
 public class Calculator extends AppCompatActivity {
@@ -17,7 +20,7 @@ public class Calculator extends AppCompatActivity {
     Button btnSix, btnSeven, btnEight, btnNine, btnZero;
     Button btnAdd, btnMinus, btnMultiply, btnDivide, btnDecimal, btnEquals;
     TextView txtResult, txtEqua, txtTest;
-    Stack<String> calcu = new Stack<String>();
+    Stack<String> calcuSeq = new Stack<String>();
     int equaLen;
     BigDecimal result; //equaLen len until sa op. 123+ is len: 4
     StringBuilder strEqua, strResult;
@@ -76,70 +79,70 @@ public class Calculator extends AppCompatActivity {
         btnOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnNumAppend(1);
+                btnNumber(1);
             }
         });
 
         btnTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnNumAppend(2);
+                btnNumber(2);
             }
         });
 
         btnThree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnNumAppend(3);
+                btnNumber(3);
             }
         });
 
         btnFour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnNumAppend(4);
+                btnNumber(4);
             }
         });
 
         btnFive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnNumAppend(5);
+                btnNumber(5);
             }
         });
 
         btnSix.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnNumAppend(6);
+                btnNumber(6);
             }
         });
 
         btnSeven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnNumAppend(7);
+                btnNumber(7);
             }
         });
 
         btnEight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnNumAppend(8);
+                btnNumber(8);
             }
         });
 
         btnNine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnNumAppend(9);
+                btnNumber(9);
             }
         });
 
         btnZero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnNumAppend(0);
+                btnNumber(0);
             }
         });
 
@@ -186,17 +189,17 @@ public class Calculator extends AppCompatActivity {
         btnEquals.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txtResult.append(""+result);
+                getInfix();
             }
         });
     }
 
     public void operator(StringBuilder str, char op) {
-        if(calcu.size() == 1) {
-            calcu.pop();
-            calcu.push(String.valueOf(result));
+        if(calcuSeq.size() == 1) {
+            calcuSeq.pop();
+            calcuSeq.push(String.valueOf(result));
         } else {
-            calcu.push(txtResult.getText().toString());
+            calcuSeq.push(txtResult.getText().toString());
         }
 
         if(!Character.isDigit(str.charAt(str.length()-1)) && str.charAt(str.length()-1) != '.') {
@@ -204,48 +207,88 @@ public class Calculator extends AppCompatActivity {
         } else {
             str.append(op);
         }
-
         txtEqua.setText(str.toString());
 
         equaLen = txtEqua.length();
     }
 
-    public void btnNumAppend(int num) {
+    public void btnNumber(int num) {
         txtResult.append(""+num);
         txtEqua.append(""+num);
 
-        if(calcu.size() == 1) {
+        if(calcuSeq.size() == 1) {
             calcuSequential(op);
         }
     }
 
     public void calcuSequential(char op) {
-        String newTermSHIT = txtEqua.getText().toString().substring(equaLen);
+        String newTerm = txtEqua.getText().toString().substring(equaLen);
 
-        BigDecimal testNum1 = new BigDecimal(calcu.peek());
-        BigDecimal testNum2 = new BigDecimal(newTermSHIT);
+        BigDecimal testNum1 = new BigDecimal(calcuSeq.peek());
+        BigDecimal testNum2 = new BigDecimal(newTerm);
 
         switch(op) {
             case '+':
                 result = testNum1.add(testNum2);
-                txtTest.append("nigana si plus");
                 break;
             case '-':
                 result = testNum1.subtract(testNum2);
-                txtTest.append("nigana si minus RESULT IS: " + result);
                 break;
             case '*':
                 result = testNum1.multiply(testNum2);
-                txtTest.setText("nigana si times");
                 break;
             case '/':
-                result = testNum1.divide(testNum2);
-                txtTest.setText("nigana si divide");
+//                // TODO: AIRTHMETICEXCEPTION SHIT
+                try {
+                    result = testNum1.divide(testNum2);
+                } catch (Exception e) {
+                    result = BigDecimal.valueOf(0);
+                    throw new ArithmeticException("real");
+                }
+                result = testNum1.divide(testNum2, 11, RoundingMode.HALF_EVEN);
+
                 break;
         }
 
         txtResult.setText("");
         txtResult.setText(String.valueOf(result));
+    }
 
+    Stack<String> MDASnum = new Stack<>();
+    Stack<String> MDASop = new Stack<>();
+    List<String> infix = new ArrayList<>();
+    @SuppressLint("SetTextI18n")
+    public void getInfix() {
+        // get txtEqua string and split it by ops and numbers
+        // store into list in order
+        // [0] = 7, [1] = +, [2] = 5, [3] = * ... etc
+        String strFullEqua = txtEqua.getText().toString();
+        txtResult.setText("og text shit: " + strFullEqua);
+
+        String[] numbers = strFullEqua.split("[+\\-*/]");
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = numbers[i].trim();
+            System.out.println(numbers[i]);
+        }
+
+        String[] operators = strFullEqua.split("[0-9]+");
+
+        for (int i = 0; i < numbers.length; i++) {
+            infix.add(numbers[i]);
+            if (i < operators.length) {
+                infix.add(operators[i]);
+            }
+        }
+
+        txtTest.append("\n"+infix);
+        txtTest.append("\nINFIX SHIT: " + infix);
+    }
+    public void calcuMDAS() {
+
+    }
+
+    public enum precedence {
+        MultDiv, AddMin;
     }
 }
